0ec92ee915c003654973f21716c5ad7fdf2811a8
Andrew Harari <andrew@tetra.team>
2022-04-26 09:04:02 -0400

    164 - tooltip

--- src/components/ToolbarButton.tsx
+++ src/components/ToolbarButton.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from 'react'
-import IconButton from '@mui/material/IconButton'
+import {IconButton, Tooltip} from '@mui/material'
 import { TToolbarComponentProps, TToolbarButtonSize } from './Toolbar'
 
 interface IToolbarButtonProps {
@@ -34,6 +34,8 @@ const ToolbarButton: FunctionComponent<IToolbarButtonProps> = (props) => {
     }
     if (props.icon) {
         return (
+            <Tooltip title={props.label}>
+
             <IconButton
                 {...sharedProps}
                 aria-label={props.label}
@@ -42,6 +44,7 @@ const ToolbarButton: FunctionComponent<IToolbarButtonProps> = (props) => {
             >
                 {props.icon}
             </IconButton>
+            </Tooltip>
         )
     }
     if (props.component) {
